eb281fec4d7934031b9f348c979ba59db1e7c82e
Subhranil Ghosh <su3010nil2001@gmail.com>
2022-09-22 23:43:29 +0530

    server.js

---
+++ backend/model/todoModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const todoSchema = mongoose.Schema({
+  text:{
+    type: String,
+    required: [true, 'Please add a text value']
+  }
+},
+{
+  timestamps: true,
+}
+)
