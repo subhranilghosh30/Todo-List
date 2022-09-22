0d63c7c9804c80aa6f4355216fe6445c6ace7d7c
Subhranil Ghosh <su3010nil2001@gmail.com>
2022-09-23 01:28:56 +0530

    server.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,43 @@ const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
 const registerUser= asyncHandler(async(req, res) =>{
-  res.json({ message: 'Register User' })
+
+  const{ name, email, password } = req.body
+
+  if(!name || !email || !password){
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
+
+  const userExists = await User.findOne({email})
+  if(userExists) {
+    res.status(400)
+    throw new Error('User Already Exists')
+  }
+
+  //hash password
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //Create user
+
+  const user= await User.create({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  if(user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    })
+  } else{
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 const loginUser= asyncHandler(async(req, res) =>{
