4bbabd1c13bbc79096b6760e208028e1af9dbbf4
Subhranil Ghosh <su3010nil2001@gmail.com>
2022-09-22 19:45:53 +0530

    server.js

--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,5 +1,6 @@
 const express= require('express')
 const router= express.Router()
+const {getToDo}= require('../controllers/todoController')
 
 router.get('/', (req, res)=> {
   res.status(200).json({ message: 'Recieved ToDo' })
--- todoController.js
+++ todoController.js
@@ -1,3 +1,7 @@
 const getToDo = (req, res) => {
   res.status(200).json({ message: 'Get ToDo'})
 }
+
+module.exports={
+  getToDo,
+}
