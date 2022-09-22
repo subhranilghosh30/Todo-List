806ec8944a487d8d9c922cb2ada8e088923d8f1c
Subhranil Ghosh <su3010nil2001@gmail.com>
2022-09-22 23:35:47 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express= require('express')
+const colors = require('colors')
 const dotenv= require('dotenv').config()
 const {errorHandler}= require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port= process.env.PORT || 4000
-
+connectDB()
 const app= express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
