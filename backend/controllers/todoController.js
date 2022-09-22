15481c69e0bd8ae0c79a44849ec5fadee1422504
Subhranil Ghosh <su3010nil2001@gmail.com>
2022-09-22 21:44:21 +0530

    server.js

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,21 +1,24 @@
-const getToDo=(req, res) => {
-  res.status(200).json({ message: 'Get Todo' })
-}
+const asyncHandler = require('express-async-handler')
 
-const setToDo=(req, res) => {
-  if(req.body.text){
-    
+const getToDo=asyncHandler(async(req, res) => {
+  res.status(200).json({ message: 'Get Todo' })
+})
+
+const setToDo=asyncHandler(async(req, res) => {
+  if(!req.body.text){
+    res.status(400)
+    throw new Error('Please add ToDo')
   }
   res.status(200).json({ message: 'Set Todo' })
-}
+})
 
-const updateToDo=(req, res) => {
+const updateToDo=asyncHandler(async(req, res) => {
 res.status(200).json({ message: `Update ToDo ${req.params.id}` })
-}
+})
 
-const deleteToDo=(req, res) => {
+const deleteToDo=asyncHandler(async(req, res) => {
 res.status(200).json({ message: `Delete ToDo ${req.params.id}` })
-}
+})
 
 module.exports={
   getToDo,
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express= require('express')
 const dotenv= require('dotenv').config()
+const {errorHandler}= require('./middleware/errorMiddleware')
 const port= process.env.PORT || 4000
 
 const app= express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/api/todo', require('./routes/todoRoutes'))
+
+app.use(errorHandler)
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
