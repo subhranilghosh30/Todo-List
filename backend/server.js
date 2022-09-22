897c4989ca4fbcb94403622bc14bc985a52eb17c
Subhranil Ghosh <su3010nil2001@gmail.com>
2022-09-23 00:42:31 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,16 @@ const dotenv= require('dotenv').config()
 const {errorHandler}= require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port= process.env.PORT || 4000
+
 connectDB()
+
 const app= express()
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
 app.use('/api/todo', require('./routes/todoRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
